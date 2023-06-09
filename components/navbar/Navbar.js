974f2f44f303611ee865aee2a2dc8e29ee33bd82
k-vernon <kvernon908@gmail.com>
2023-06-08 23:54:05 -0400

    create nav function

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
