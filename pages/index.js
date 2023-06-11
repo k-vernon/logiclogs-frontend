bf470a318c8ab8e710bb34508dc90bb195320ab6
k-vernon <kvernon908@gmail.com>
2023-06-10 23:35:52 -0400

    edit landing hero

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ export default function Home() {
     <div>
       <Navbar/>
       <div className="pt-24 pl-8">
-        <h1>Easy way to get full control of your projects</h1>
+        <h1 className="text-4xl font-bold mb-4">Easy way to get full control of your projects</h1>
+        <p className="text-lg mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in cursus elit, eget consequat quam. Vestibulum varius pharetra enim a sagittis.</p>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-md">Get Started</button>
       </div>
       
     </div>
