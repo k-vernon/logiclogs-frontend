f01f751f862196cb932dc76f53662efd7d824c12
k-vernon <kvernon908@gmail.com>
2023-07-02 22:32:09 -0400

    add nav for dash

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link"
 
 
 export default function DashboardPage() {
   return <div>
     <h1>Dashboard</h1>
+    <section className="">
+      Dash Nav
+      <Link href='/'>
+      Home
+      </Link>
+    </section>
     </div>
 }
