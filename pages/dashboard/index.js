108be0ca090174ecf97251a50a6ab7303e57fb0d
k-vernon <kvernon908@gmail.com>
2023-07-10 23:41:26 -0400

    add sections to dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function DashboardPage() {
   return (
     <div className="flex">
-      <nav className="w-64 bg-gray-200">
+      <nav className="w-40 bg-gray-200">
         <ul className="flex flex-col h-screen w-10">
           <li className="p-4">
             <Link className="text-gray-800 hover:text-gray-600" href="/">
@@ -23,7 +23,13 @@ export default function DashboardPage() {
           </li>
         </ul>
       </nav>
-      <div className="flex-1">
+      <div className="flex-row p-12 w-screen h-screen bg-blue-200">
+        <section className="bg-red-200 w-28 h-28">
+          <h1>Welcome user</h1>
+        </section>
+        <section className="">
+          <h1>Welcome user</h1>
+        </section>
         <section className="">
           <h1>Welcome user</h1>
         </section>
