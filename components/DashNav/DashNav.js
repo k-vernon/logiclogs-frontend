3be24a222db5da7fc4242ebec49a4ea7be64fe51
k-vernon <kvernon908@gmail.com>
2023-07-22 23:58:21 -0400

    change nav position

--- components/DashNav/DashNav.js
+++ components/DashNav/DashNav.js
@@ -5,8 +5,8 @@ import Link from "next/link"
 
 export default function DashNav(){
   return (
-    <nav className="flex w-40 bg-gray-200 py-20 p-4  h-screen w-10 justify-center text-center">
-      <ul className="flex flex-col justify-between">
+    <nav className="flex w-40 bg-gray-200 py-20 p-4 h-screen w-10 text-center">
+      <ul className="flex flex-col justify-around">
         <li className="">
         Logo
         </li>
